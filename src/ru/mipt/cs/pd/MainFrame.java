1a55ec93861985f2b5cfca34a7a88c2f33ccef6d
lily-valley <s.pakhomova7@mail.ru>
2014-05-21 18:07:49 +0400

    Update MainFrame.java

--- src/ru/mipt/cs/pd/MainFrame.java
+++ src/ru/mipt/cs/pd/MainFrame.java
@@ -1,16 +1,20 @@
 package ru.mipt.cs.pd;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
 
+import javax.swing.AbstractAction;
+import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -21,17 +25,25 @@ import javax.swing.JTextArea;
 import javax.swing.KeyStroke;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultEditorKit;
 import javax.swing.text.DefaultHighlighter;
-import javax.swing.text.Highlighter;
 import javax.swing.text.DefaultHighlighter.DefaultHighlightPainter;
-import javax.swing.text.Highlighter.HighlightPainter;
+import javax.swing.text.Document;
+import javax.swing.text.Highlighter;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 
+import ru.mipt.cs.pd.dna.AddFeature;
 import ru.mipt.cs.pd.dna.Environment;
 import ru.mipt.cs.pd.dna.Feature;
+import ru.mipt.cs.pd.dna.MakeComplement;
 import ru.mipt.cs.pd.dna.SimpleExtract;
 import ru.mipt.cs.pd.io.InputFile;
+import ru.mipt.cs.pd.io.NewFile;
 import ru.mipt.cs.pd.io.SaveFile;
 import ru.mipt.cs.pd.primers.FRMSimplePrimers;
 import ru.mipt.cs.pd.restrictases.EnzymeSelector;
@@ -44,21 +56,23 @@ public class MainFrame extends JFrame implements ActionListener{
 	private JButton btnPrimers;
 	private JButton btnRestrictaza;
 	private JButton btnCertificate;
-	protected JTextArea txtMain;
+	protected static JTextArea txtMain;
 	private JScrollPane scrlMain;
 	private JFileChooser fc;
 	private MainFrame Self=this;
+	private JScrollPane forFeatures;
+	private JList<String> list;
+	private DefaultListModel<String> listModel;
 
 	// objects
 	private SimpleExtract passed;
 
 	JMenuBar menuBar;
 	JMenu menu, submenu;
-	JMenuItem menuItemOpen, menuItemNew, menuItemSave, menuItemFeature;
+	JMenuItem menuItemOpen, menuItemNew, menuItemSave, menuItemFeature, menuItemComplement;
 	JRadioButtonMenuItem rbMenuItem;
 	JCheckBoxMenuItem cbMenuItem;
 
-	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -71,17 +85,15 @@ public class MainFrame extends JFrame implements ActionListener{
 			}
 		});
 	}
-	
 
 	private void initComponents() {
-
 		contentPane = new JPanel();
 		setContentPane(contentPane);
 		btnPrimers = new JButton();
 		btnRestrictaza = new JButton();
 		btnCertificate = new JButton();
 
-		fc=new JFileChooser();
+		fc = new JFileChooser();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Master of primers");
@@ -94,42 +106,46 @@ public class MainFrame extends JFrame implements ActionListener{
 				EnzymeSelector e = new EnzymeSelector();
 			}
 		});
-		btnCertificate.setText("Открыть справку");
+		btnCertificate.setText("Open reference"); //справка
 		btnCertificate.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				// btnCertificateActionPerformed(evt);
 			}
 		});
-
 	}
 
 	public MainFrame() {
-
-		initComponents();
-
-		setBounds(100, 100, 450, 300);
-
 		
-
-		txtMain = new JTextArea(InputFile.zy);          //TODO
+		initComponents();
+		this.setExtendedState(MAXIMIZED_BOTH);
+		txtMain = new JTextArea(InputFile.zy);          
 		txtMain.setLineWrap(true);
 
 		scrlMain = new JScrollPane(txtMain);
 		scrlMain.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrlMain.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		
+		listModel = new DefaultListModel();
+		list = new JList<String>(listModel);
+		forFeatures = new JScrollPane(list);
+	    forFeatures.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+	    forFeatures.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);		
+		
+		showFeatures();
 
-		// ///////////////////////
-		// creating the menu
+	    
+	    forFeatures.setMaximumSize(new Dimension(100000,45));
+	    forFeatures.setMinimumSize(new Dimension(10,45));	    
 		
 		// Create the menu bar.
 		menuBar = new JMenuBar();
 
 		// Build the first menu.
-		menu = new JMenu("Меню");
+		menu = new JMenu("Menu");
 		menu.setMnemonic(KeyEvent.VK_A);
 		menuBar.add(menu);
 
-		menuItemOpen = new JMenuItem("Открыть файл", KeyEvent.VK_T);
+		menuItemOpen = new JMenuItem("Open file", KeyEvent.VK_T);
 		menuItemOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,
 				ActionEvent.CTRL_MASK));
 		menu.add(menuItemOpen);
@@ -139,13 +155,18 @@ public class MainFrame extends JFrame implements ActionListener{
 			}
 		});
 
-		menuItemNew = new JMenuItem("Создать новый");
+		menuItemNew = new JMenuItem("Create new");
 		menuItemNew.setMnemonic(KeyEvent.VK_B);
 		menuItemNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,
 				ActionEvent.CTRL_MASK));
 		menu.add(menuItemNew);
+		menuItemNew.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				NewFile.OpenNewFile();   
+			}
+		});
 
-		menuItemSave = new JMenuItem("Сохранить изменения");
+		menuItemSave = new JMenuItem("Save changes");
 		menuItemSave.setMnemonic(KeyEvent.VK_H);
 		menuItemSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,
 				ActionEvent.CTRL_MASK));
@@ -158,77 +179,115 @@ public class MainFrame extends JFrame implements ActionListener{
 		});
 
 		// Build second menu "Edition"
-		menu = new JMenu("Редактирование");
+		menu = new JMenu("Editing");
 		menu.setMnemonic(KeyEvent.VK_N);
 		JMenuItem menuItem = null;
 		
-		menuItemFeature = new JMenuItem("Создать новую Feature");
+		menuItemFeature = new JMenuItem("Create new Feature");
 		menuItemFeature.setMnemonic(KeyEvent.VK_Z);
 		menuItemFeature.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F,
 				ActionEvent.CTRL_MASK));
 		menu.add(menuItemFeature);
 		menuItemFeature.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent arg1) {
-				
+				int begin = txtMain.getSelectionStart();
+				int end = txtMain.getSelectionEnd();
+				AddFeature x = new AddFeature();
+				Feature res;
+				res = x.CreateFeature(begin, end);
+				highlightAFeature(res);
+				InputFile.feature.add(res);
 			}
-			
+		});
+		
+		menuItemComplement = new JMenuItem("Paste Complement");
+		menuItemComplement.setMnemonic(KeyEvent.VK_Z);
+		menuItemComplement.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P,
+				ActionEvent.CTRL_MASK));
+		menu.add(menuItemComplement);
+		menuItemFeature.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg1) {
+				int begin = txtMain.getSelectionStart();
+				int end = txtMain.getSelectionEnd();
+				String zy = txtMain.getText();
+				MakeComplement.ComplementExtract(begin, end, zy);        //TODO
+			}
 		});
 		
         menuItem = new JMenuItem(new DefaultEditorKit.CutAction());
-        menuItem.setText("Вырезать");
+        menuItem.setText("Cut");
         menuItem.setMnemonic(KeyEvent.VK_T);
         menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,
 				ActionEvent.CTRL_MASK));
         menu.add(menuItem);
 
         menuItem = new JMenuItem(new DefaultEditorKit.CopyAction());
-        menuItem.setText("Копировать");
+        menuItem.setText("Copy");
         menuItem.setMnemonic(KeyEvent.VK_C);
         menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C,
 				ActionEvent.CTRL_MASK));
         menu.add(menuItem);
 
         menuItem = new JMenuItem(new DefaultEditorKit.PasteAction());
-        menuItem.setText("Вставить");
+        menuItem.setText("Paste");
         menuItem.setMnemonic(KeyEvent.VK_P);
         menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V,
 				ActionEvent.CTRL_MASK));
         menu.add(menuItem);
-
+        
+        //////////////////////////////////////////// undo
+       	final UndoManager undo = new UndoManager();
+		Document doc = txtMain.getDocument();
+		doc.addUndoableEditListener(new UndoableEditListener() {
+		     public void undoableEditHappened(UndoableEditEvent evt) {
+		           undo.addEdit(evt.getEdit());
+		     }
+		 });
+		 txtMain.getActionMap().put("Undo",
+		  new AbstractAction("Undo") {
+		     public void actionPerformed(ActionEvent evt) {
+		            try {
+		               if (undo.canUndo()) {
+		                       undo.undo();
+		                }
+		            } catch (CannotUndoException e) {
+		     }
+		  }
+		 });
+		 txtMain.getInputMap().put(KeyStroke.getKeyStroke("control Z"), "Undo");
+         ////////////////////////////////////////////////////////////////////// redo
+		 txtMain.getActionMap().put("Redo",
+			        new AbstractAction("Redo") {
+			            public void actionPerformed(ActionEvent evt) {
+			                try {
+			                    if (undo.canRedo()) {
+			                        undo.redo();
+			                    }
+			                } catch (CannotRedoException e) {
+			                }
+			            }
+			        });
+		txtMain.getInputMap().put(KeyStroke.getKeyStroke("control Y"), "Redo");
+        ////////////////////////////////////////////////////////////////////////
 		menuBar.add(menu);
 		this.setJMenuBar(menuBar);
 		// ///////////////////////////////////	
 		
-		// describing open and save buttons
-		final String newline = "\n";
-
-		///////////////////////////////
-
-		contentPane.add(btnPrimers);
-		contentPane.add(scrlMain);
-
 		btnPrimers.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
-
 				Environment.setMainDNA(txtMain.getText());
-				
 				SwingUtilities.invokeLater(new Runnable() {
 					public void run() {
 						int i, j;
 						i = txtMain.getSelectionStart();
 						j = txtMain.getSelectionEnd();
 						passed = new SimpleExtract(i, j);
-
 						RenzymeMass restrictionEnzymesNearGene = new RenzymeMass();
-
 						FRMSimplePrimers x = new FRMSimplePrimers(passed, restrictionEnzymesNearGene); 
 					}
 				});
-
 			}
 		});
-		
-		
 
 		// DESIGN
 		GroupLayout layout = new GroupLayout(contentPane);
@@ -242,6 +301,7 @@ public class MainFrame extends JFrame implements ActionListener{
 		HORGroup.addComponent(btnPrimers);
 		HORGroup.addComponent(btnRestrictaza);
 		HORGroup.addComponent(btnCertificate);
+		HORGroup.addComponent(forFeatures);
 		HORGroup.addComponent(scrlMain);
 		layout.setHorizontalGroup(HORGroup);
 
@@ -249,22 +309,17 @@ public class MainFrame extends JFrame implements ActionListener{
 		VertGroup.addComponent(btnPrimers);
 		VertGroup.addComponent(btnRestrictaza);
 		VertGroup.addComponent(btnCertificate);
+		VertGroup.addComponent(forFeatures);
 		VertGroup.addComponent(scrlMain);
 		layout.setVerticalGroup(VertGroup);
 	}
 
-
-	@Override
 	public void actionPerformed(ActionEvent e) {
-		 
         //Handle open button action.
         if (e.getSource() == menuItemOpen) {
-        	System.out.println("some");
             int returnVal = fc.showOpenDialog(Self);
- 
             if (returnVal == JFileChooser.APPROVE_OPTION) {
                 File file = fc.getSelectedFile();
-                //This is where a real application would open the file.
                 String name = file.getName();
                 String path = file.getPath();
             	String format="";
@@ -278,6 +333,7 @@ public class MainFrame extends JFrame implements ActionListener{
             	}
             	InputFile.OpenFile(format, path, txtMain);
             	highlightFeatures();
+            	showFeatures();
             	}
             }
         }
@@ -295,6 +351,15 @@ public class MainFrame extends JFrame implements ActionListener{
 	        }
 	  }
 	}
+	
+	public void highlightAFeature(Feature buf){
+		Highlighter h = txtMain.getHighlighter();
+		DefaultHighlightPainter p = new DefaultHighlighter.DefaultHighlightPainter(Color.blue);
+	    	try {
+	          h.addHighlight(buf.getBegin(), buf.getEnd(), p);
+	        } catch (BadLocationException exc) {       }	  
+	 
+	}
 	private void initColors(DefaultHighlightPainter[] painters) {
 		painters[0] = new DefaultHighlighter.DefaultHighlightPainter(Color.gray);
 		painters[1] = new DefaultHighlighter.DefaultHighlightPainter(Color.pink);
@@ -302,7 +367,6 @@ public class MainFrame extends JFrame implements ActionListener{
 		painters[3] = new DefaultHighlighter.DefaultHighlightPainter(Color.orange);
 		painters[4] = new DefaultHighlighter.DefaultHighlightPainter(Color.yellow);
 	}
-
 
 	public void highlightEnzymes(SimpleExtract[] array){
 		Highlighter h = txtMain.getHighlighter();
@@ -318,4 +382,14 @@ public class MainFrame extends JFrame implements ActionListener{
 	  }
 	}
 	
-	}    
+	private void showFeatures(){
+		int numberOfFeatures = InputFile.feature.size();
+		String feat[] = new String[numberOfFeatures];
+		for (int count = 0; count < numberOfFeatures; count++){
+			feat[count] = InputFile.feature.get(count).toString();
+			System.out.println(feat[count]);
+			listModel.addElement(feat[count]);
+		}
+	}
+	
+}    
